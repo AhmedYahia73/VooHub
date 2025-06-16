e070bc655eab62082387867b1580497a01c8fdf7
Ahmed <Wegostationdev@gmail.com>
2025-06-16 14:32:12 +0200

    commit

--- app/Http/Controllers/Api/Admin/AdminRequestController.php
+++ app/Http/Controllers/Api/Admin/AdminRequestController.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Storage;
 
 class AdminRequestController extends Controller
 {
-
-
     public function getAllRequest(){
         $request = ModelsRequest::
         where('status', 'pending')
